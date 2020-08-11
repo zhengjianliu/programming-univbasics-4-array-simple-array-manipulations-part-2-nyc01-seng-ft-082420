96c57699493be7ebe869b5fe088ab0b1a2536ea4
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-11 23:40:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_concat(firstArray,secondArray)
+  firstArray.concat(secondArray)
+  firstArray
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+  array
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+  array
+end
+
+def using_delete_at(array,i)
+  array.delete_at(i)
+end
